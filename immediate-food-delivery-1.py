dcf8e991c6f508fb69070806a47238f4a11bf82c
Madhumitha Ichapuram <1madhu2mitha@gmail.com>
2023-09-11 14:56:03 +0300

    Create immediate-food-delivery-1.py

---
+++ immediate-food-delivery-1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame({'immediate_percentage': [
+        round(delivery[delivery['order_date'] == delivery['customer_pref_delivery_date']].size / delivery.size * 100, 2)
+    ]})
