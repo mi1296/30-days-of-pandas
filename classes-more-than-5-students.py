5d71903a9e7ab95ecf14364d579b3e8ac14a4fc0
Madhumitha Ichapuram <1madhu2mitha@gmail.com>
2023-09-11 14:49:45 +0300

    Create classes-more-than-5-students.py

---
+++ classes-more-than-5-students.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    stats = courses.groupby(['class']).count().reset_index()
+    return stats[stats['student'] >= 5][['class']]
